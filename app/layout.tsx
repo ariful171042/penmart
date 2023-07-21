4c67266b5aa9111317631cd2adc5d8734b34f0d5
ariful171042 <arif171042@gmail.com>
2023-07-21 21:35:47 +0600

    setup done

---
+++ app/layout.tsx
@@ -0,0 +1,22 @@
+import './globals.css'
+import type { Metadata } from 'next'
+import { Poppins } from 'next/font/google'
+
+const inter = Poppins({ subsets: ['latin'],weight:["100","200","300","400","500","600","700","800","900"], })
+
+export const metadata: Metadata = {
+  title: 'pick Now | Home',
+  description: 'E-Commere Website',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  )
+}
